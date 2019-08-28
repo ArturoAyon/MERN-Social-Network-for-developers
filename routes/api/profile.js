eec9a1112496124c14d4d0600db255205d6e3f03
Arturo <arturo.al@hotmail.com>
2019-08-27 20:53:09 -0600

    Add DELETE /api/profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -123,12 +123,32 @@ router.get('/', async (req, res) => {
 router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
-      user: req.params.user_id
+      user: req.user_id
     }).populate('user', ['name', 'avatar']);
 
     if (!profile) return res.status(400).json({ msg: 'Profile not found' });
 
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind == 'ObjectId')
+      return res.status(400).json({ msg: 'Profile not found' });
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    DELETE api/profile/
+// @desc     Delete profile, user $ posts
+// @access   Public
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
   } catch (err) {
     console.error(err.message);
 
